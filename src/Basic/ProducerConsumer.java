1372a05b7dcaedbc0102934d8497577c236714cf
weibowang <weibo.webber.wang@gmail.com>
2016-11-18 23:58:55 -0500

    yt

--- src/Basic/ProducerConsumer.java
+++ src/Basic/ProducerConsumer.java
@@ -63,6 +63,8 @@ public class ProducerConsumer {
 //		c2.start();
 //		c3.start();		
 		
+		
+		
 		Thread p = new Thread(new Producer5(pq));
 		Thread p2 = new Thread(new Producer5(pq));
 		Thread p3 = new Thread(new Producer5(pq));
